2912b2ad8433801ca4a72c8685a30f1e5f64398a
CelebanPatrycja <114691633+CelebanPatrycja@users.noreply.github.com>
2023-07-13 11:47:12 +0200

    update

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,80 +1,28 @@
-// Plik: cat-api.js
-
 import axios from 'axios';
 
-// Ustawienie klucza dostępu w nagłówku HTTP dla wszystkich żądań
-axios.defaults.headers.common['x-api-key'] =
-  'live_YawJLsUjmnQm8rwbY3zEDv7xzzZC7eUCMJC6xUYQE16krBAMqyPYecmZ3GM5IP94';
+let axiosCatApi = null;
 
-function fetchBreeds() {
-  return new Promise((resolve, reject) => {
-    const url = 'https://api.thecatapi.com/v1/breeds';
-
-    // Wyświetlanie animacji ładowania
-    const loader = document.querySelector('p.loader');
-    loader.style.display = 'block';
-
-    // Ukrywanie listy ras i ewentualnego błędu
-    const breedSelect = document.querySelector('select.breed-select');
-    breedSelect.style.display = 'none';
-    const error = document.querySelector('p.error');
-    error.style.display = 'none';
-
-    axios
-      .get(url)
-      .then(response => {
-        const breeds = response.data.map(breed => ({
-          value: breed.id,
-          label: breed.name,
-        }));
-
-        // Ukrywanie animacji ładowania i wyświetlanie listy ras
-        loader.style.display = 'none';
-        breedSelect.style.display = 'block';
-
-        resolve(breeds);
-      })
-      .catch(error => {
-        // Ukrywanie animacji ładowania i wyświetlanie błędu
-        loader.style.display = 'none';
-        error.style.display = 'block';
-        reject(error);
-      });
-  });
+//config function
+export function init(api_key) {
+  axiosCatApi = require('axios').default;
+  axiosCatApi.defaults.headers.common['x-api-key'] = api_key;
 }
 
-function fetchCatByBreed(breedId) {
-  return new Promise((resolve, reject) => {
-    const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
-
-    // Wyświetlanie animacji ładowania
-    const loader = document.querySelector('p.loader');
-    loader.style.display = 'block';
-
-    // Ukrywanie informacji o kocie i ewentualnego błędu
-    const catInfo = document.querySelector('div.cat-info');
-    catInfo.style.display = 'none';
-    const error = document.querySelector('p.error');
-    error.style.display = 'none';
-
-    axios
-      .get(url)
-      .then(response => {
-        const cat = response.data[0];
-
-        // Ukrywanie animacji ładowania i wyświetlanie informacji o kocie
-        loader.style.display = 'none';
-        catInfo.style.display = 'block';
-
-        resolve(cat);
-      })
-      .catch(error => {
-        // Ukrywanie animacji ładowania i wyświetlanie błędu
-        loader.style.display = 'none';
-        error.style.display = 'block';
-        reject(error);
-      });
-  });
+//capturing array of all objects from the server
+export function fetchBreeds() {
+  return axiosCatApi
+    .get(`https://api.thecatapi.com/v1/breeds`)
+    .then(response => response.data)
+    .catch(error => {
+      console.error(error);
+    });
 }
-
-export { fetchBreeds, fetchCatByBreed };
+//capturing 1 object from the server
+export function fetchCatByBreed(breedId) {
+  return axiosCatApi
+    .get(`https://api.thecatapi.com/v1/images/${breedId}`)
+    .then(response => response.data)
+    .catch(error => {
+      console.error(error);
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,80 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import * as catApi from './cat-api';
+import SlimSelect from 'slim-select';
+import '../node_modules/slim-select/dist/slimselect.css';
+import Notiflix from 'notiflix';
 
 const breedSelect = document.querySelector('.breed-select');
-const catInfoBox = document.querySelector('.cat-info');
+const catInfo = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
 
-fetchBreeds(breedSelect);
+const apiKey =
+  'live_YawJLsUjmnQm8rwbY3zEDv7xzzZC7eUCMJC6xUYQE16krBAMqyPYecmZ3GM5IP94';
 
-breedSelect.addEventListener('change', breedChange);
+breedSelect.addEventListener('change', handleChange);
+breedSelect.classList.add('hidden');
+error.classList.add('hidden');
 
-// obsługa stanu ładowania - wyboru rasy
-// w trakcie żądanie HTTP wyswietlana jest animacja ładowania - element p.loader
-
-function breedChange(event) {
-  loader.classList.remove('is-hidden');
-  catInfoBox.classList.add('is-hidden');
-  fetchCatByBreed(event.target.value);
+function addList(items) {
+  const markup = items
+    .map(item => {
+      return `<option value=${item.reference_image_id}>${item.name} </option>`;
+    })
+    .join('');
+  breedSelect.innerHTML = markup;
 }
 
-export { breedSelect, catInfoBox, loader, error };
+//creating post on website with cat name, image and descripion
+function addPost(item) {
+  const markup = `
+  <img class="postImage" src="${item.url}" alt="">
+  <div>
+    <h1>${item.breeds[0].name}</h1>
+    <p>${item.breeds[0].description}</p>
+    <p><b>Temperament: </b>${item.breeds[0].temperament}</p>
+  </div>
+  `;
+  catInfo.innerHTML = markup;
+}
+
+function handleChange(event) {
+  loader.classList.remove('hidden');
+  catInfo.classList.add('hidden');
+  catApi
+    .fetchCatByBreed(event.currentTarget.value)
+    .then(function (response) {
+      addPost(response);
+      loader.classList.add('hidden');
+      catInfo.classList.remove('hidden');
+      error.classList.add('hidden');
+    })
+    .catch(function (error) {
+      Notiflix.Notify.failure(
+        'Upps! Coś poszło nie tak. Odśwież stronę jeszcze raz.'
+      );
+      error.classList.remove('hidden');
+      loader.classList.add('hidden');
+      console.log(error);
+    });
+}
+
+catApi.init(apiKey);
+catApi
+  .fetchBreeds()
+  .then(function (response) {
+    addList(response);
+    loader.classList.add('hidden');
+    breedSelect.classList.remove('hidden');
+    error.classList.add('hidden');
+    const select = new SlimSelect({
+      select: breedSelect,
+    });
+  })
+  .catch(function (error) {
+    Notiflix.Notify.failure(
+      'Oops! Something went wrong! Try reloading the page!'
+    );
+    error.classList.remove('hidden');
+    loader.classList.add('hidden');
+    console.log(error);
+  });
